29f40cc7821f89535d5b5bcac5ada487b2009620
Frank Morton <fmorton@mac.com>
2026-05-21 10:43:44 -0400

    rename robot-python-xbox-controller

---
+++ examples/xbox_controller_example.py
@@ -0,0 +1,21 @@
+import pygame
+
+from robot.xbox_controller import XboxController
+
+controller = XboxController().connect()
+
+print("Controller connected")
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.JOYAXISMOTION:
+            print(f"Controller axis {event.axis} value {event.value}")
+        elif event.type == pygame.JOYBUTTONDOWN:
+            print(f"Button {event.button} pressed on controller {event.joy}")
+
+            if event.button == XboxController.BUTTON_XBOX:
+                running = False
---
+++ examples/xbox_controller_state_example.py
@@ -0,0 +1,18 @@
+from robot.xbox_controller import XboxController
+from time import sleep
+
+
+def robot(controller):
+    print("robot", controller.state.state_string())
+
+    if controller.state.button_down_milliseconds(XboxController.BUTTON_XBOX):
+        return False
+
+    sleep(XboxController.EVENT_LOOP_DELAY)
+
+    return True
+
+
+controller = XboxController().connect()
+
+controller.run(robot)
